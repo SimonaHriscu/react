8022994160b99d0c49adc144d4c2f39532da378f
Simona <see.moona@yahoo.com>
2020-06-09 10:52:23 +0200

    npx create-react-app

---
+++ CLASS/09.06.20 intro/first-app/src/js/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import logo from '../svg/logo.svg';
+
+function App() {
+  return (
+    <div>
+      <p>Welcome to your first React file</p>
+      <form>
+        <input type="text" placeholder="write something" />
+      </form>
+      <img src="./img/dribble1.jpg" alt="photo"></img>
+    </div>
+  );
+}
+
+export default App;
