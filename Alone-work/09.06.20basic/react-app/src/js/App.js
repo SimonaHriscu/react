d9c8221b0640664cccde984fbc70ec619bd8b19f
Simona <see.moona@yahoo.com>
2020-06-09 12:00:57 +0200

    first set-up

---
+++ Alone-work/09.06.20basic/react-app/src/js/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          This is the second React file
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
