0e1393fc74f35c8269986b86e0d8d8be0887d667
Simona <see.moona@yahoo.com>
2020-06-10 16:18:53 +0200

    react with scss no npx create-react-app

---
+++ CLASS/10.06.20 react with sass/src/js/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>This is the second React file</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
