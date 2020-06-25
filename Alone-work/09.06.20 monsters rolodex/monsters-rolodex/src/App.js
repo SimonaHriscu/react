555944acd9fb229f8bbcfb22ea71e363ec9f10f0
Simona <see.moona@yahoo.com>
2020-06-25 16:54:09 +0200

    filter added

--- Alone-work/09.06.20 monsters rolodex/monsters-rolodex/src/App.js
+++ Alone-work/09.06.20 monsters rolodex/monsters-rolodex/src/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { SearchBox } from "./components/search-box/search-box.component";
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monster: [
-        { name: "Frankenstein" },
-        { name: "Dracula" },
-        { name: "Zombie" }
-      ],
+      monsters: [],
+      searchField: "",
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={e => 
+            this.setState({ searchField: e.target.value })
+          }
+        />
+
+        <CardList monsters={filteredMonsters}></CardList>
+
         {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p> {/* rendering the constructor here */}
-          {/* <button onClick={() => this.setState({ string: "Hello Hriscu" })}>
+          <p>{this.state.string} </p>
+          <button onClick={() => this.setState({ string: "Hello Hriscu" })}>
             Change text
-          </button> 
+          </button>
         </header> */}
       </div>
     );
