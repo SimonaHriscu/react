516aee48340444b837771439c80e8c20ab286e1d
Simona <see.moona@yahoo.com>
2020-07-02 17:20:12 +0200

    action in the parent

--- CLASS/02.06.20-redux-simplified/src/components/ChildComponent.js
+++ CLASS/02.06.20-redux-simplified/src/components/ChildComponent.js
@@ -1,15 +1,15 @@
 import React, { Component, Fragment } from "react";
 import {connect} from 'react-redux';
-import {increment, decrement, changeName} from "../actions";
+import {increment, decrement} from "../actions";
 
 class ChildComponent extends Component {
     render() {
      // console.log(this.props.store);
-      const {store, increment, decrement, changeName} = this.props;
+      const {store, increment, decrement} = this.props;
       // console.log(this.props); // this will print store, increment, decrement, changeName 
       return (
         <Fragment>
-         <button onClick={()=> changeName()}>Change name</button>
+         
          <hr/>
          Our current likes: {store.counter} 
          <hr/>
@@ -25,7 +25,6 @@ const mapStateToProps= (store)=>{};
 const mapDispatchToProps={
   increment,
   decrement, 
-  changeName,
 }
 
   export default connect(mapStateToProps,mapDispatchToProps)(ChildComponent);
--- CLASS/02.06.20-redux-simplified/src/components/ParentComponent.js
+++ CLASS/02.06.20-redux-simplified/src/components/ParentComponent.js
@@ -1,16 +1,18 @@
 import React, { Component, Fragment } from "react";
 import ChildComponent from "./ChildComponent";
 import { connect } from "react-redux";
+import {changeName} from "../actions";
 
 class ParentComponent extends Component {
   render() {
     //console.log(this.props.store);
-    const { store } = this.props;
+    const { store, changeName } = this.props;
   //  console.log(store); // this will print counter:0 and name:"Simona"
 
     return (
       <Fragment>
         <h4>Hey, {store.name}!</h4>
+        <button onClick={()=> changeName()}>Change name</button>
 
         <ChildComponent store={store} propsName="Nancy" />
       </Fragment>
@@ -22,5 +24,8 @@ const mapStateToProps = (store) => {
     store,
   };
 };
+const mapDispatchToProps={
+  changeName,
+}
 
-export default connect(mapStateToProps)(ParentComponent);
+export default connect(mapStateToProps,mapDispatchToProps)(ParentComponent);
