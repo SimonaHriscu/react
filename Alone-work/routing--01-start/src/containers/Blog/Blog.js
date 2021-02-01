e05469dff93ca97d80a82498552685d9e3b58787
Simona <see.moona@yahoo.com>
2021-02-01 18:58:55 +0100

    route parameters

--- Alone-work/routing--01-start/src/containers/Blog/Blog.js
+++ Alone-work/routing--01-start/src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 import './Blog.css';
-
+import FullPost from './FullPost/FullPost';
 class Blog extends Component {
   render() {
     return (
@@ -32,7 +32,8 @@ class Blog extends Component {
           </nav>
         </header>
         <Route path="/" exact component={Posts} />
-        <Route path="/new-post" exact component={NewPost} />
+        <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
